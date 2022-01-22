89262087b328e1653b69c41374c775a85282dcfe
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-01-22 14:59:54 +0100

    Add files via upload

---
+++ kalendarz.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+  <head>
+
+     <meta charset="utf-8">
+     <title>Mój kalendarz</title>
+     <link rel="stylesheet" href="styl5.css">
+
+
+  </head>
+  <body>
+    <div id="ban1">
+        <img src="logo1.png" alt="Mój Kalendarz">
+    </div>
+    <div id="ban2"></div>
+    <div id="glowny"></div>
+    <div id="stopka"></div>
+    
+  
+
+  </body>
+
+</html>
