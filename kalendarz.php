5faa4037db362319c89058e07b77e0cdef9f1441
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-01-22 15:31:35 +0100

    Add files via upload

--- kalendarz.php
+++ kalendarz.php
@@ -15,22 +15,48 @@
     </div>
     <div id="ban2">
         <h1>Kalendarz</h1>
+   </div>
+  <main>
         <?php
         
         
-        
+        require("connect.php");
+
+        $qr = "SELECT dataZadania, miesiac, wpis FROM `zadania` WHERE miesiac = 'lipiec'";
+        $result = $conn->query($qr);
+        while ($row = $result->fetch_assoc()) {
+            $data = $row['dataZadania'];
+            $miesiac = $row['miesiac'];
+            $wpis = $row['wpis'];
+
+            echo '<div>';
+            echo "<h6>$data, $miesiac</h6>";
+            echo "<p>$wpis</p>";
+            echo '</div>';
+        }
+
+        mysqli_close($conn)
         
         
         
         
         ?>
-    </div>
+      </main>  
+  
     <div id="glowny">
         <?php
         
         
         
+        require("connect.php");
+
+    if (isset($_POST['wydarzenie'])) {
         
+        $wydarzenie = $_POST['wydarzenie'];
+        $qr = "UPDATE zadania SET wpis = '$wydarzenie' WHERE dataZadania = '2020-07-13'";
+        $conn->query($qr);
+        $conn->close();
+    }
         
         
         
@@ -39,17 +65,17 @@
         ?>
     </div>
     <div id="stopka">
-        <form method="post" >
+        <form action="kalendarz.php" method="post">
             
-        dodaj wpis<input type="text">
-        DODAJ <input type="button">
+        dodaj wpis<input type="text" name="wpiss">
+            <button type="submit">DODAJ</button>
+            <br>
            <p>Stronę wykonał:00000000</p>   
             
             
             
             
-        </form>
-        
-    </div>
+        </form> 
+      </div>
     </body>
 </html>
