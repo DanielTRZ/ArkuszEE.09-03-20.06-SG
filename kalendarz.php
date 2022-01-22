f97fba6c99c005f6a73666f4345f8c8d836646fe
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-01-22 15:04:12 +0100

    Add files via upload

--- kalendarz.php
+++ kalendarz.php
@@ -13,12 +13,44 @@
     <div id="ban1">
         <img src="logo1.png" alt="Mój Kalendarz">
     </div>
-    <div id="ban2"></div>
-    <div id="glowny"></div>
-    <div id="stopka"></div>
-    
-  
+    <div id="ban2">
+        <h1>Kalendarz</h1>
+        <?php
+        
+        
+        
+        
+        
+        
+        
+        ?>
+    </div>
+    <div id="glowny">
+        <?php
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        ?>
+    </div>
+    <div id="stopka">
+        <form method="post" >
+            
+        dodaj wpis<input type="text">
+        DODAJ <input type="button">
+           <p>Stronę wykonał:00000000</p>   
+            
+            
+            
+            
+        </form>
+        
+    </div>
 
-  </body>
-
+    </body>
 </html>
