c9b53c81da49d199e424c0b2efad1463356feb32
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-05-22 00:11:57 +0200

    Update kalendarz.php

--- kalendarz.php
+++ kalendarz.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pl">
 <head>
-     <meta charset="utf-8">
+<meta charset="utf-8">
      <title>Mój kalendarz</title>
      <link rel="stylesheet" href="styl5.css">
   </head>
